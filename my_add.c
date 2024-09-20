f0552f3a01d275d0d56851238ba8be493337cf0d
itayG98 <itai.g98@gmail.com>
2024-09-20 16:02:17 +0300

    !

--- my_add.c
+++ my_add.c
@@ -18,10 +18,13 @@
  */
 int main()
 {
-    unsigned int a;
-    printf("Please insert unsigned number.\n");
-    scanf("%u", &a);
+    unsigned int a, b, sum;
+    printf("Please insert two decimal unsigned number seperated by spaces.\n");
+    scanf("%u  %u", &a, &b);
     print_binary_with_mask(a);
+    print_binary_with_mask(b);
+    sum = sum_numbers(a, b);
+    print_binary_with_mask(sum);
     printf("Press any key to continue...\n");
     _getch();
     return 1;
@@ -55,4 +58,42 @@ int print_binary_with_mask(unsigned toPrint)
     }
     printf("\n");
     return 1;
+}
+
+/**
+ * @brief Computes the sum of two unsigned integers using bitwise operations.
+ *
+ * This function adds two unsigned integers without using the '+' operator.
+ * It utilizes bitwise operations to calculate the sum and handle carry bits,
+ * iterating through each bit position until all bits are processed.
+ *
+ * @param a The first unsigned integer to be added.
+ * @param b The second unsigned integer to be added.
+ * @return The sum of the two unsigned integers as an unsigned integer.
+ *
+ * Example usage:
+ * @code
+ *    unsigned int x = 13;
+ *    unsigned int y = 34;
+ *    unsigned int result = sum_numbers(x, y);
+ *  result will be 47 in binary representation : 00000000000000000000000000101111
+ * @endcode
+ */
+int sum_numbers(unsigned int a, unsigned int b)
+{
+    unsigned i, shift, carry, a_bit, b_bit, sum_bit, res;
+    i = carry = res = 0;
+    shift = 1;
+    while (i < sizeof(a) * 8)
+    {
+        a_bit = shift & a;
+        b_bit = shift & b;
+        sum_bit = (a_bit ^ b_bit) ^ carry;
+        res |= (sum_bit & shift);
+        carry = (a_bit & b_bit) | (a_bit & carry) | (b_bit & carry);
+        shift <<= 1;
+        carry <<= 1;
+        i++;
+    }
+    return res;
 }
